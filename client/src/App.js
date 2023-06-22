ac5072f91ffcaed8281117c1df3fac6a0a6cb295
Ashish <ashishbdhane@gmail.com>
2023-06-22 18:45:34 +0530

    ReduxToolkit Config

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
-import Login from './components/Login/Login';
+import {Navigate, RouterProvider, createBrowserRouter} from 'react-router-dom';
+import HomePage from './pages/homePage/index.js';
+import LoginPage from './pages/loginPage/index.js';
+import ProfilePage from './pages/profilePage/index.js';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <LoginPage />,
+		children: [
+			{
+				path: 'home',
+				element: <HomePage />,
+			},
+			{
+				path: 'profile/:userId',
+				element: <ProfilePage />,
+			},
+		],
+	},
+]);
 
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+	return (
+		<div className="app">
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
 export default App;
